0d428b612dfa2e7a70b0ee9291591f8f6d3a97c5
John Wismar <john.wismar@clearcapital.com>
2016-05-23 15:12:40 -0700

    added content-type and accept header to solr modder

--- src/main/java/com/clearcapital/oss/cassandra/annotation_processors/SolrCoreModifier.java
+++ src/main/java/com/clearcapital/oss/cassandra/annotation_processors/SolrCoreModifier.java
@@ -32,7 +32,7 @@ public class SolrCoreModifier extends TableProcessor<SolrCoreModifier> {
     }
 
     public void create() throws CommandExecutionException, AssertException, CassandraException {
-        action(CREATE,null);
+        action(CREATE, null);
     }
 
     public void reloadDontReindex() throws CommandExecutionException, AssertException, CassandraException {
@@ -50,7 +50,7 @@ public class SolrCoreModifier extends TableProcessor<SolrCoreModifier> {
     public void unload() throws CommandExecutionException, AssertException, CassandraException {
         Map<String, String> queryParams = new TreeMap<String, String>();
         queryParams.put("deleteAll", "true");
-        action(UNLOAD,null);
+        action(UNLOAD, null);
     }
 
     private void reload(boolean reindex, boolean deleteAllData)
@@ -58,23 +58,26 @@ public class SolrCoreModifier extends TableProcessor<SolrCoreModifier> {
         Map<String, String> queryParams = new TreeMap<String, String>();
         queryParams.put("reindex", reindex ? "true" : "false");
         queryParams.put("deleteAll", deleteAllData ? "true" : "false");
-        action(RELOAD,queryParams);
+        action(RELOAD, queryParams);
     }
 
-    private void action(String action,Map<String,String> parameters) throws CommandExecutionException, AssertException, CassandraException {
+    private void action(String action, Map<String, String> parameters)
+            throws CommandExecutionException, AssertException, CassandraException {
         URI uri = getSession().getSolrCoreAdminUri(tableName, action, null);
         log.info("url: " + uri.toASCIIString());
 
         UriBuilder builder = UriBuilder.fromUri(uri);
         if (parameters != null && !parameters.isEmpty()) {
-        for (Map.Entry<String, String> param : parameters.entrySet()) {
-            builder.queryParam(param.getKey(), param.getValue());
+            for (Map.Entry<String, String> param : parameters.entrySet()) {
+                builder.queryParam(param.getKey(), param.getValue());
+            }
         }
-        }
-        //HttpGet httpGet = new HttpGet(builder.build());
+        // HttpGet httpGet = new HttpGet(builder.build());
         HttpPost httpPost = new HttpPost(builder.build());
 
         HttpCommand command = HttpCommand.builder().setRequest(httpPost).build();
+        httpPost.addHeader("Accept", "application/json");
+        httpPost.addHeader("Content-Type", "application/json");
         executor.addCommand(command);
     }
 
