d6066b52824dfcb42768fcd51b2440482ef46061
Early Ehlinger <earlye@gmail.com>
2016-05-23 08:03:41 -0500

    CCP-5060 - cleaned imports

--- src/main/java/com/clearcapital/oss/cassandra/test_support/CassandraTestResource.java
+++ src/main/java/com/clearcapital/oss/cassandra/test_support/CassandraTestResource.java
@@ -10,7 +10,6 @@ import org.junit.rules.ExternalResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.clearcapital.oss.cassandra.RingClient;
 import com.clearcapital.oss.cassandra.annotation_processors.CassandraTableProcessor;
 import com.clearcapital.oss.cassandra.annotation_processors.SolrCoreModifier;
 import com.clearcapital.oss.cassandra.annotations.CassandraTable;
